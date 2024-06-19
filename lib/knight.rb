c4756450def88af993f9de34276747e0b402be00
SeanMero <MeroFromVero@gmail.com>
2024-06-19 16:21:19 +0800

    Add movement capability for knight

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,8 +1,31 @@
+require_relative "board"
+
 # generates a knight with a position and a certain movement capability
-class Knight
+class Knight < Board
   attr_accessor :position
 
-  def initialize(position = [0,0])
+  def initialize(position = [0, 0])
     @position = position
+  end
+
+  def valid_move?(from, to) # rubocop:disable Metrics/AbcSize,Metrics/CyclomaticComplexity,Metrics/PerceivedComplexity
+    if (to[0] == from[0] + 1 && to[1] == from[1] + 2) ||
+       (to[0] == from[0] + 2 && to[1] == from[1] + 1) ||
+       (to[0] == from[0] + 2 && to[1] == from[1] - 1) ||
+       (to[0] == from[0] + 1 && to[1] == from[1] - 2) ||
+       (to[0] == from[0] - 1 && to[1] == from[1] - 2) ||
+       (to[0] == from[0] - 2 && to[1] == from[1] - 1) ||
+       (to[0] == from[0] - 2 && to[1] == from[1] + 1) ||
+       (to[0] == from[0] - 1 && to[1] == from[1] + 2)
+      true
+    end
+  end
+
+  def on_board?(to)
+    true if (0..(size - 1)).include?(to[0]) && (0..(size - 1)).include?(to[1])
+  end
+
+  def move(to, from = position)
+    @position = to if valid_move?(from, to) && on_board?(to)
   end
 end
